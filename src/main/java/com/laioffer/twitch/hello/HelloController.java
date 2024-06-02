1fcd396916322ee2c13f25ed8f83b6697339607c
Jiaqi Liu <estelle.liu233@gmail.com>
2024-06-02 12:35:10 -0400

    created hello controller

--- src/main/java/com/laioffer/twitch/hello/HelloController.java
+++ src/main/java/com/laioffer/twitch/hello/HelloController.java
@@ -1,4 +1,13 @@
 package com.laioffer.twitch.hello;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello World!";
+    }
 }
